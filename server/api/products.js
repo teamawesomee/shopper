bdb3f1fd7e3485577395488f0f9329f6160b0d91
rachelefine <rachelefine@gmail.com>
2018-04-17 16:45:37 -0400

    added back routes for updating and deleting specific products cuz i should have had these already

--- server/api/products.js
+++ server/api/products.js
@@ -19,3 +19,20 @@ router.get('/:productId', (req, res, next) => {
     .then(product => res.json(product))
     .catch(next)
 });
+
+router.put('/:productId', (req, res, next) => {
+  Product.findById(req.params.productId)
+    .then(product => product.update(req.body))
+    .then(product => res.json(product))
+    .catch(next);
+});
+
+router.delete('/:productId', (req, res, next) => {
+  Product.findById(req.params.productId)
+    .then(product => product.destroy(req.body))
+    .then(() => res.status(202).json('deleted!'))
+    .catch(next);
+});
+
+
+
