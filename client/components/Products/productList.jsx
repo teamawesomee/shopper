debe93361ba2b3a112c84e92ef5a7f8309b7823f
Richi Thampan <richithampan@gmail.com>
2018-04-17 17:41:01 -0400

    fixed filter

--- client/components/Products/productList.jsx
+++ client/components/Products/productList.jsx
@@ -5,7 +5,7 @@ class ProductList extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      products: [{name: 'A Rock', category: 'cat1', description: 'Your basic rock', quantity: '15', price: '$6.72'}, {name: 'A Hard Place', category: 'cat1', description: 'sounds way more appealing than the rock', quantity: '6', price: '$6.72'}, {name: 'My Mom', category: 'cat1', description: 'Insert mom joke here', quantity: '6', price: '$6.72'}, {name: 'YOUR mom', category: 'cat2', description: 'insert another mom joke here', quantity: '6', price: '$6.72'}, {name: 'A dick in a box', category: 'cat2', description: 'Step One: cut a hole in the box; Step two: put your junk in that box', quantity: '6', price: 'fucking priceless'}, {name: 'sassy comeback 101', category: 'cat2', description: 'Step One: cut a hole in the box; Step two: put your junk in that box', quantity: '6', price: '$16.88'}],
+      products: [{name: 'A Rock', category: ['cat1'], description: 'Your basic rock', quantity: '15', price: '$6.72'}, {name: 'A Hard Place', category: ['cat1'], description: 'sounds way more appealing than the rock', quantity: '6', price: '$6.72'}, {name: 'My Mom', category: ['cat1'], description: 'Insert mom joke here', quantity: '6', price: '$6.72'}, {name: 'YOUR mom', category: ['cat2'], description: 'insert another mom joke here', quantity: '6', price: '$6.72'}, {name: 'A dick in a box', category: ['cat2'], description: 'Step One: cut a hole in the box; Step two: put your junk in that box', quantity: '6', price: 'fucking priceless'}, {name: 'sassy comeback 101', category: ['cat2'], description: 'Step One: cut a hole in the box; Step two: put your junk in that box', quantity: '6', price: '$16.88'}],
       searchValue: '',
       selectedCategory: 'All'
     }
@@ -33,9 +33,14 @@ catHandler(event){
   render() {
     let products = this.state.products
     if (this.state.selectedCategory != 'All'){
-      products = this.state.products.filter(product => product.category === this.state.selectedCategory)
+      products = this.state.products.filter(product => product.category.includes(this.state.selectedCategory))
     }
-    const prodCategories = Array.from(new Set(this.state.products.map(prod => prod.category)))
+    let helper = []
+    let prodCategories = this.state.products.map(prod => prod.category)
+    for (var i = 0; i < prodCategories.length; i++){
+      helper = helper.concat(prodCategories[i])
+    }
+    prodCategories = Array.from(new Set(helper))
     console.log("categories: ", prodCategories)
     const searchValue = this.state.searchValue;
     const regExSearchValue = new RegExp(searchValue, "gi")
