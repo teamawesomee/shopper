8fd180a73e3c31d4e8e3e271502fa4e6e00a071f
rockycodes <rachelefine@gmail.com>
2018-04-17 10:43:31 -0400

    Products backend (#7)
    * done with product model and routes
    
    * deleted repeat seed file

--- server/api/products.js
+++ server/api/products.js
@@ -1,9 +1,15 @@
-const router = require("express").Router();
-const { Product } = require("../db/models");
+const router = require('express').Router();
+const { Product } = require('../db/models');
 module.exports = router;
 
-router.get("/", (req, res, next) => {
+router.get('/', (req, res, next) => {
   Product.findAll()
     .then(products => res.json(products))
     .catch(next);
 });
+
+router.get('/:productId', (req, res, next) => {
+  Product.findById(req.params.productId)
+    .then(product => res.json(product))
+    .catch(next)
+})
