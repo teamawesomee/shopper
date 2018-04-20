8931ea7ee92c4a3e56567c49fc155734bea00347
Richi Thampan <richithampan@gmail.com>
2018-04-20 15:26:54 -0400

    fixing file name

--- server/db/models/index.js
+++ server/db/models/index.js
@@ -1,6 +1,6 @@
 const User = require('./user')
 const Product = require(`./product`)
-const Order = require('./Order')
+const Order = require('./order')
 
 /**
  * If we had any associations to make, this would be a great place to put them!
